fc0828e9a4a4350792fe9af8e6e91ccadd839d7c
Nicolas Thery <nthery@gmail.com>
2014-07-17 15:17:45 +0200

    Initial Commit

---
+++ SwiftForth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftForth
+//
+//  Created by Nicolas Thery on 17/07/14.
+//  Copyright (c) 2014 Nicolas Thery. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
